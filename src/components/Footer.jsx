1346448af3f53deca79586a03263c83c19b23258
Lakshan Sandeepa <lakshansandeepa0305@gmail.com>
2026-05-06 12:09:05 +0530

    Enhance footer component: add footer icons for improved visual consistency and update contact info styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -24,8 +24,8 @@ const Footer = () => {
     <footer className="footer">
       <div className="footer-contents">
         {/* footer logo */}
-        <div className="footer-card footer-logo">
-          <a href="/#home" class="logo">
+        <div className="footer-card ">
+          <a href="#home" className="header-logo footer-logo">
             <div className="logo-circle">E</div>educa<span>.</span>
           </a>
           <p>
@@ -58,37 +58,37 @@ const Footer = () => {
           <ul>
             <li>
               <NavHashLink smooth to="/#resources">
-                <FaCaretRight /> Academic Resources
+                <FaCaretRight className="footer-icon" /> Academic Resources
               </NavHashLink>
             </li>
             <li>
               <NavHashLink smooth to="/#classes">
-                <FaCaretRight /> Class Schedule
+                <FaCaretRight className="footer-icon" /> Class Schedule
               </NavHashLink>
             </li>
             <li>
               <NavHashLink smooth to="/#teachers">
-                <FaCaretRight /> Our Faculty
+                <FaCaretRight className="footer-icon" /> Our Faculty
               </NavHashLink>
             </li>
             <li>
               <NavHashLink smooth to="/#about">
-                <FaCaretRight /> Why Choose
+                <FaCaretRight className="footer-icon" /> Why Choose
               </NavHashLink>
             </li>
             <li>
               <NavHashLink smooth to="/#contacts">
-                <FaCaretRight /> Contact Support
+                <FaCaretRight className="footer-icon" /> Contact Support
               </NavHashLink>
             </li>
             <li>
               <NavHashLink smooth to="/#reviews">
-                <FaCaretRight /> Students Feedbacks
+                <FaCaretRight className="footer-icon" /> Students Feedbacks
               </NavHashLink>
             </li>
             <li>
               <a href="https://www.doenets.lk/examresults">
-                <FaCaretRight /> O/L Results
+                <FaCaretRight className="footer-icon" /> O/L Results
               </a>
             </li>
           </ul>
@@ -100,22 +100,22 @@ const Footer = () => {
           <ul>
             <li>
               <Link to="/tools">
-                <FaCaretRight /> Smart Study Tools
+                <FaCaretRight className="footer-icon" /> Smart Study Tools
               </Link>
-            </li>{" "}
+            </li>
             <li>
               <Link to="/quizzes">
-                <FaCaretRight /> Interactive Quizzes
+                <FaCaretRight className="footer-icon" /> Interactive Quizzes
               </Link>
             </li>{" "}
             <li>
               <Link to="/paper-hub">
-                <FaCaretRight /> Past & Target Papers
+                <FaCaretRight className="footer-icon" /> Past & Target Papers
               </Link>
             </li>{" "}
             <li>
               <Link to="/daily-questions">
-                <FaCaretRight /> Daily Mission
+                <FaCaretRight className="footer-icon" /> Daily Mission
               </Link>
             </li>{" "}
           </ul>{" "}
@@ -124,19 +124,18 @@ const Footer = () => {
         <div className="footer-card footer-contact">
           <h3>Contact Info</h3>
           <p>
-            <FaPhone />{" "}
             <a href="tel:+94 77 123 4567" className="contact-link">
-              +94 77 123 4567
+              <FaPhone className="footer-icon" /> +94 77 123 4567
             </a>
           </p>
           <p>
-            <FaEnvelope />{" "}
             <a href="mailto:support@educa.lk" className="contact-link">
-              support@educa.lk
+              <FaEnvelope className="footer-icon" /> support@educa.lk
             </a>
           </p>
           <p>
-            <FaLocationDot /> 123, Education Lane, Colombo
+            <FaLocationDot className="footer-icon" /> 123, Education Lane,
+            Colombo
           </p>
 
           <div
@@ -148,7 +147,15 @@ const Footer = () => {
             }}>
             <p style={{ fontSize: "0.8rem" }}>
               For subject specific help, use our{" "}
-              <Link to="/#contact-form" style={{ color: "#ff4b2b" }}>
+              <Link
+                to="/#contact-form"
+                style={{
+                  color: "#ff4b2b",
+                  backgroundColor: "#ffefec",
+                  padding: "6px 12px",
+                  textAlign: "center",
+                  borderRadius: "5px",
+                }}>
                 Contact Form
               </Link>
             </p>
@@ -179,20 +186,20 @@ const Footer = () => {
         <div className="footer-card badge-levelup">
           <h3>for Sri Lanka | O/L Students</h3>
           <p>
-            <FaRocket />
+            <FaRocket className="footer-icon" />
             Version 1.0
           </p>
           <p>
-            <FaSquareCheck />
+            <FaSquareCheck className="footer-icon" />
             Verified Links
           </p>
           <p>
-            <FaLaptopCode />
+            <FaLaptopCode className="footer-icon" />
             last updated
             <span> 2026 April</span>
           </p>
           <p>
-            <FaUsers /> Community reviewed
+            <FaUsers className="footer-icon" /> Community reviewed
           </p>
         </div>
 
@@ -202,8 +209,7 @@ const Footer = () => {
             <FaRegCopyright /> {currentYear} All rights reserved.
           </p>
           <p>
-            This template is made with{" "}
-            <FaHeart style={{ color: "#ff4b2b", margin: "0 5px" }} /> by
+            This template is made with <FaHeart /> by
             <a
               href="http://github.com/lakshan-beast"
               target="_blank"
