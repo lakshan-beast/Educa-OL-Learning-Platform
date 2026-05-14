e29ba6f2c646576790c4ba2c72e2b9947184a5cc
Lakshan Sandeepa <lakshansandeepa0305@gmail.com>
2026-05-14 23:29:26 +0530

    Add completeShedules: aggregate class data from multiple weekly classes

---
+++ src/data/completeShedule.js
@@ -0,0 +1,15 @@
+import { allClassData11 } from "./classes/weeklyClasses11";
+import { allClassData10 } from "./classes/weeklyClasses10";
+import { allClassData9 } from "./classes/weeklyClasses9";
+import { allClassData8 } from "./classes/weeklyClasses8";
+import { allClassData7 } from "./classes/weeklyClasses7";
+import { allClassData6 } from "./classes/weeklyClasses6";
+
+export const completeShedules = [
+  ...allClassData11,
+  ...allClassData10,
+  ...allClassData9,
+  ...allClassData8,
+  ...allClassData7,
+  ...allClassData6,
+];
