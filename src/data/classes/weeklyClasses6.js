fc14f6faaa4d33c0ae257cad80e76ff7e7797089
Lakshan Sandeepa <lakshansandeepa0305@gmail.com>
2026-05-14 23:29:39 +0530

    Add allClassData6: introduce grade 6 class data including subjects, schedules, and notices

---
+++ src/data/classes/weeklyClasses6.js
@@ -0,0 +1,48 @@
+export const allClassData6 = [
+  // --- GRADE 6 CLASSES ---
+  {
+    id: "m6",
+    grade: "6",
+    subject: "O/L Mathematics",
+    teacher: "Sir 01",
+    day: "Saturday",
+    startTime: "08:00", // 24-hour format වලින් දාන්න (HH:MM)
+    endTime: "12:00",
+    statusOverride: "Auto", // "Auto", "Canceled", "Holiday"
+    link: "zoom.us",
+    notice: {
+      lesson: "Hi",
+      specialNote: "රූලක් සහ කවකටුවක් අනිවාර්යයෙන් රැගෙන එන්න.",
+    },
+  },
+  {
+    id: "s6",
+    grade: "6",
+    subject: "O/L Science",
+    teacher: "Sir 02",
+    day: "Sunday",
+    startTime: "13:30",
+    endTime: "17:30",
+    statusOverride: "Auto",
+    link: "zoom.us",
+    notice: {
+      lesson: "රසායනික විද්‍යාව - මවුලය පාඩම",
+      specialNote: "පසුගිය සතියේ නිබන්ධනය සම්පූර්ණ කර තබාගන්න.",
+    },
+  },
+  {
+    id: "e6",
+    grade: "6",
+    subject: "O/L English",
+    teacher: "Sir 03",
+    day: "Saturday",
+    startTime: "13:30",
+    endTime: "17:30",
+    statusOverride: "Auto",
+    link: "zoom.us",
+    notice: {
+      lesson: "රසායනික විද්‍යාව - මවුලය පාඩම",
+      specialNote: "පසුගිය සතියේ නිබන්ධනය සම්පූර්ණ කර තබාගන්න.",
+    },
+  },
+];
