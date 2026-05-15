4a145bad6da7339cd9144f5e321c244efa6019f1
Lakshan Sandeepa <lakshansandeepa0305@gmail.com>
2026-05-15 10:00:31 +0530

    Refactor grade11All to include student IDs and PINs as objects

--- src/data/students/grade11/all_classes.js
+++ src/data/students/grade11/all_classes.js
@@ -1,6 +1,12 @@
+// export const grade11All = [
+//   "EDU-MES-11-LAKSHAN-0512",
+//   "EDU-MES-11-USER-0123",
+//   "EDU-MES-11-NIMAL-0322",
+//   "EDU-MES-11-WIHANGA-4567",
+// ];
+
+// 💡 දැන් ID එක ඇතුළේ PIN එක නැහැ! PIN එක තියෙන්නේ වෙනමමයි.
 export const grade11All = [
-  "EDU-MES-11-LAKSHAN-0512",
-  "EDU-MES-11-USER-0123",
-  "EDU-MES-11-NIMAL-0322",
-  "EDU-MES-11-WIHANGA-4567",
+  { id: "EDU-MES-11-LAKSHAN-0512", pin: "9945" }, // ළමයා කැමති PIN එකක් දාන්න පුළුවන්
+  { id: "EDU-MES-11-KASUN-0123", pin: "1234" },
 ];
