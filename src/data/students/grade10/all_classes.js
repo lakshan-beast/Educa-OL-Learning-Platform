f39099cd95e1af8f798232bd92a15265ff5ea3dc
Lakshan Sandeepa <lakshansandeepa0305@gmail.com>
2026-05-18 12:27:05 +0530

    Add grade10 student data with IDs and passwords

---
+++ src/data/students/grade10/all_classes.js
@@ -0,0 +1,12 @@
+// export const grade11All = [
+//   "EDU-MES-11-LAKSHAN-0512",
+//   "EDU-MES-11-USER-0123",
+//   "EDU-MES-11-NIMAL-0322",
+//   "EDU-MES-11-WIHANGA-4567",
+// ];
+
+// 💡 දැන් ID එක ඇතුළේ PIN එක නැහැ! PIN එක තියෙන්නේ වෙනමමයි.
+export const grade10All = [
+  { id: "EDU-MES-10-LAKSHAN-0512", password: "lakshan1234" },
+  { id: "EDU-MES-10-TEACHER-0123", password: "teachers1234" },
+];
