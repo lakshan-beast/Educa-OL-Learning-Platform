ee1b71a8d116c2de9ad8c1065a2d1112e230beb9
Lakshan Sandeepa <lakshansandeepa0305@gmail.com>
2026-05-20 21:00:54 +0530

    Uncomment student ID in grade11Dual for inclusion in the list

--- src/data/students/grade11/dual_classes.js
+++ src/data/students/grade11/dual_classes.js
@@ -1,4 +1,6 @@
 export const grade11Dual = [
-  "EDU-ME-11-SADUNI-0412", // Maths + English
+  // "EDU-ME-11-SADUNI-0412", // Maths + English
   "EDU-MS-11-NIMAL-0822", // Maths + Science
+
+  { id: "EDU-ME-11-SADUNI-0412", password: "saduni1234" },
 ];
