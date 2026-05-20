49d7278812b377b110aa0c593bc8a9873bbf0eb2
Lakshan Sandeepa <lakshansandeepa0305@gmail.com>
2026-05-20 20:55:57 +0530

    Fix premiumStudentsList: uncommented student ID for inclusion in the list

--- src/data/students/premiumStudents.js
+++ src/data/students/premiumStudents.js
@@ -1,7 +1,7 @@
 export const premiumStudentsList = [
   // "EDU-MES-11-LAKSHAN-0305", // මේ ළමයා සල්ලි ගෙවලා තියෙන්නේ
   "EDU-M-11-KASUN-0914",
-  // "EDU-MES-11-LAKSHAN-0512",
+  "EDU-MES-11-LAKSHAN-0512",
   "EDU-MES-11-USER-0123",
   "EDU-MES-11-NIMAL-0322",
   "EDU-MES-11-WIHANGA-4567",
