543570b1ba54a09ff77947b101663c0594d842d9
Lakshan Sandeepa <lakshansandeepa0305@gmail.com>
2026-05-25 11:08:22 +0530

    premium close

--- src/data/students/premiumStudents.js
+++ src/data/students/premiumStudents.js
@@ -1,9 +1,9 @@
 export const premiumStudentsList = [
   // "EDU-MES-11-LAKSHAN-0305", // මේ ළමයා සල්ලි ගෙවලා තියෙන්නේ
-  "EDU-M-11-KASUN-0914",
+  // "EDU-M-11-KASUN-0914",
   "EDU-MES-11-LAKSHAN-0512",
-  "EDU-MES-11-USER-0123",
-  "EDU-MES-11-NIMAL-0322",
-  "EDU-MES-11-WIHANGA-4567",
-  "EDU-MES-11-TEACHER-0123",
+  // "EDU-MES-11-USER-0123",
+  // "EDU-MES-11-NIMAL-0322",
+  // "EDU-MES-11-WIHANGA-4567",
+  // "EDU-MES-11-TEACHER-0123",
 ];
