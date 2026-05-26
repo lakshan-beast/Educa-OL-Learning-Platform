344f0f4c8a8929cc7d737b4355d43ab230377cd6
Lakshan Sandeepa <lakshansandeepa0305@gmail.com>
2026-05-26 09:27:53 +0530

    add new student

--- src/data/students/grade10/dual_classes.js
+++ src/data/students/grade10/dual_classes.js
@@ -1,4 +1,5 @@
 export const grade10Dual = [
   "EDU-ME-10-SADUNI-0412", // Maths + English
   "EDU-MS-10-NIMAL-0822", // Maths + Science
+  { id: "EDU-MS-10-WIHANGANIMSARA-0987", password: "wihanga1234" },
 ];
